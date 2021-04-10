bab1bd4652d0f8b580f37ac5a4023c33bdd913ce
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-04-10 13:17:12 -0400

    Add appendFile function under fs

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 // HTML string logic goes here
 
 // FS logic goes here
+// Imports fs system
+const fs = require(fs);
+
+// REPLACE htmlString with whatever variable is created
+fs.appendFile("index.html", htmlString, (err) => {
+    err ? console.error(err) : console.log('File created successfully.');
+});
