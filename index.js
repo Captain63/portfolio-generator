26192e31be95a4dbc64db2d273013a50945d7fce
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-04-10 13:45:55 -0400

    added inquire logic

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 // Inquirer logic goes here
+const inquirer = require('inquirer');
+
+const mapQA = new Map();
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'What is your location?',
+        name: 'location'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub URL?',
+        name: 'gitURL'
+    }
+])
+    .then((response) => {
+        mapQA.set('name', response.name);
+        mapQA.set('location', response.location);
+        mapQA.set('gitURL', response.gitURL);
+        
+        //Remaining steps go here...
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 // HTML string logic goes here
 
